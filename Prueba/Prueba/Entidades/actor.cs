1cd98f94c1ad44a6b1bf2b49e79b339e58d72dc2
Carlos Guagrilla <carlos.guagrilla@hotmail.com>
2022-01-24 18:08:57 -0500

    sunida

---
+++ Prueba/Prueba/Entidades/actor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba.Entidades
+{
+    public class actor
+    {
+        public int actor_id { get; set; }
+        public string firts_name { get; set; }
+        public string last_name { get; set; }
+        public DateTime last_update { get; set; }
+
+    }
+}
---
+++ Prueba/PruebaDB/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace PruebaDB
+{
+    public class Class1 : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder opciones)
+        {
+            opciones.UseSqlServer("Server= CARLOS ; initial catalog = dvd; trusted_connection = true; ");
+
+            //opciones.UseNpgsql(" Host = localhost; Database = Insti; Username = postgres; Password = carlos00; ");
+
+        }
+    }
+}
